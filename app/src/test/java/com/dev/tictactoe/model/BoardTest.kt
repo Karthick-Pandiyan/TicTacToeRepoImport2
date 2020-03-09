8552d0d1b7bbcdf085b15fcbcd68e75accc8fb1f
2020-DEV-058 <>
2020-03-09 15:13:41 +0530

    Test board is returning expected player, when player two called.

--- app/src/test/java/com/dev/tictactoe/model/BoardTest.kt
+++ app/src/test/java/com/dev/tictactoe/model/BoardTest.kt
@@ -5,13 +5,23 @@ import org.junit.Test
 
 class BoardTest {
     private val playerOne = "John"
-    private val game = Board(playerOne)
+    private val playerTwo = "Harry"
+    private val game = Board(playerOne, playerTwo)
 
     @Test
     fun `Given function should return expected player, when player one called`(){
         val expectedResult = playerOne
 
         val actualResult = game.playerOne
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Given function should return expected player, when player two called`(){
+        val expectedResult = playerTwo
+
+        val actualResult = game.playerTwo
 
         Assert.assertEquals(expectedResult, actualResult)
     }
