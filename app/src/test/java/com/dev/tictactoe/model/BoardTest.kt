1aa5f887554b0e7dbaa15930b6e2661461f02ecd
2020-DEV-058 <>
2020-03-09 18:12:45 +0530

    Test board is returning expected result, when player have same values in diagonal cells from left.

--- app/src/test/java/com/dev/tictactoe/model/BoardTest.kt
+++ app/src/test/java/com/dev/tictactoe/model/BoardTest.kt
@@ -98,4 +98,16 @@ class BoardTest {
 
         Assert.assertTrue(actualResult)
     }
+
+    @Test
+    fun `Given function should return expected result, if it has three same diagonal cells from Left`() {
+
+        val cell = Cell(game.player1)
+        game.cells[0][0] = cell
+        game.cells[1][1] = cell
+        game.cells[2][2] = cell
+        val actualResult = game.isWinnerAvailable()
+
+        Assert.assertTrue(actualResult)
+    }
 }
