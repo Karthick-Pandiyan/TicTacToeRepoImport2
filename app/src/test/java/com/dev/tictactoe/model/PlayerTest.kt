52f0deb7827d4be69dc3122da302f01bfc392514
2020-DEV-058 <>
2020-03-09 15:07:15 +0530

    Test given function should return expected result, when player values are equal

--- app/src/test/java/com/dev/tictactoe/model/PlayerTest.kt
+++ app/src/test/java/com/dev/tictactoe/model/PlayerTest.kt
@@ -5,13 +5,23 @@ import org.junit.Test
 
 class PlayerTest {
     private val playerName = "John"
-    private val player1 = Player(playerName)
+    private val playerValue = "X"
+    private val player1 = Player(playerName, playerValue)
 
     @Test
     fun `Given function should return expected result, when player names are equal`(){
 
         val actualResult = player1.name
         val expectedResult = playerName
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Given function should return expected result, when player values are equal`(){
+
+        val actualResult = player1.value
+        val expectedResult = playerValue
 
         Assert.assertEquals(expectedResult, actualResult)
     }
